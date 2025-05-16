8c501b8c656321801510fb244428d8e4a4b4689c
naveen <naveenagrawal1008@gmail.com>
2025-05-16 11:52:15 +0530

    changed the variable name __dirname.

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import { app,server } from "./lib/socket.js";
 dotenv.config()
 
 const PORT = process.env.PORT
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({limit: '50mb'}))
 app.use(cookieparser())
@@ -30,10 +30,10 @@ app.use("/api/messages",messageRoutes)
 
 
 if(process.env.NODE_ENV ===   "production"){
-    app.use(express.static(path.join(_dirname,'../frontend/dist')));
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.join(_dirname,'../frontend','dist','index.html'));
+        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'));
     });
 
 }
